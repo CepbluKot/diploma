6f39400f25866aa88867c99cfd638c21b666049f
Igor <igorcat100@gmail.com>
2024-05-09 18:03:13 +0300

    wip

--- gui/calls/set_params_calls.py
+++
@@ -1,8 +0,0 @@
-from servo_realization.output import robot
-
-
-def set_speed_call(value):
-    robot.speed = float(value)
-
-    print('speed set', robot.speed)
-    robot.send_all_axis_speeds(float(value))
--- gui/output.py
+++ gui/output.py
@@ -2,10 +2,10 @@ import typing
 from tkinter.ttk import Frame, Notebook, Treeview
 from tkinter import *
 
-from gui.controls_tab.controls_tab import controls_tab
-from gui.connection_config_tab.connection_config_tab import connection_config_tab
+from gui.controls_tab import controls_tab
+from gui.connection_config_tab import connection_config_tab
+from gui.map_tab import map_tab
 
-axis_set_angle_slider_data = {}
 
 def init_gui():
     window = Tk()
@@ -15,15 +15,17 @@ def init_gui():
     tab_control = Notebook(window)  
     tab1 = Frame(tab_control)  
     tab2 = Frame(tab_control)
+    tab3 = Frame(tab_control)
 
     
     tab_control.add(tab1, text='controls_tab')  
     tab_control.add(tab2, text='connection_config')
+    tab_control.add(tab3, text='map')
     tab_control.pack(expand=1, fill='both')  
 
     
     controls_tab(tab1)
     connection_config_tab(tab2)
-
+    map_tab(tab3)
     
     window.mainloop()
